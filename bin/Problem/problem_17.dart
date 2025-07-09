e86295fafc407dceffad15ec57772acea9aa4b9a
miel2793 <mahmudsifat2793@gmail.com>
2025-07-09 22:58:30 +0600

    add

---
+++ bin/Problem/problem_17.dart
@@ -0,0 +1,13 @@
+main()
+{
+  for(int i=1;i<=100;i++)
+    {
+      if(i==41)
+        {
+          continue;
+        }
+      print(i);
+
+    }
+}
+//Write a dart program to print 1 to 100 but not 41.
