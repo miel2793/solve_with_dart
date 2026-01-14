0b8c5a65d20e11a4587a8e70e5496bb85cbc4620
miel2793 <mahmudsifat2793@gmail.com>
2026-01-14 20:52:53 +0600

    add

--- bin/Problem/user_input.dart
+++ bin/Problem/user_input.dart
@@ -2,11 +2,15 @@ import 'dart:io';
 
 main()
 {
- String? name;
- print("Enter Your Name: ");
-  name = stdin.readLineSync();
-  print(" My name Is  $name");
-  int  ? a= int.tryParse(stdin.readLineSync()!) ;
-  print(a);
+   String ? name;
+     print(" Enter Your  Name: ");
+     name= stdin.readLineSync();
+       String ? dept;
+        print("Enter Your Department: ");
+        dept= stdin.readLineSync();
+         int  ? id;
+           id = int.parse(stdin.readLineSync()!);
+           print('$name\n$id\n$dept');
+
 
 }
