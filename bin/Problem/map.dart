ea0cb0726e3878e494a7e62f34e21a13826d2316
miel2793 <mahmudsifat2793@gmail.com>
2026-01-15 11:05:39 +0600

    add

--- bin/Problem/map.dart
+++ bin/Problem/map.dart
@@ -6,6 +6,12 @@ main()
         'ID': "20234103368",
         "Department": " Cse "
       };
+  masifa["Name"]="A nas";
   print( masifa);
+  // set
+  Set s ={ 'miel','afo','kfli'};
+  s.add('afi');
+
+  print(s.elementAt(2));
 
 }
