861a3fd5173587eb5ca063c34c743384aaca7a31
miel2793 <mahmudsifat2793@gmail.com>
2026-01-15 11:15:11 +0600

    add

---
+++ bin/Problem/Condition.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+
+main()
+{
+  String name;
+   print(" Enter Your Name : ");
+     name = stdin.readLineSync()!;
+  int age;
+   print("Enter Your age ");
+    age = int.parse(stdin.readLineSync()!);
+
+    if ( name=="Miel" && age<=21)
+      {
+        print("This Is Right Person ");
+      }
+     else if (name=='Anas' && age>=23)
+       {
+         print(' Bad boy ');
+       }
+}
