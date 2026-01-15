23efe54869dc3d394897a90728c560c8955792db
miel2793 <mahmudsifat2793@gmail.com>
2026-01-15 10:49:50 +0600

    add

---
+++ bin/Problem/map.dart
@@ -0,0 +1,11 @@
+main()
+{
+  Map<String,String> masifa =
+      {
+         "Name": "MD: Miel Mahmud Sifat",
+        'ID': "20234103368",
+        "Department": " Cse "
+      };
+  print( masifa['Name']);
+
+}
