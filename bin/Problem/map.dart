bbe05bbb9e9af7a7b1ee8be30e0316697205c977
miel2793 <mahmudsifat2793@gmail.com>
2026-01-16 11:25:46 +0600

    add

--- bin/Problem/map.dart
+++ bin/Problem/map.dart
@@ -12,23 +12,29 @@ main()
   Set s ={ 'miel','afo','kfli'};
   s.add('afi');
 
-  var  miel =[{'Name':'Miel Mahmud Sifat','Product':'Apple',"Quantity":'200'},
-    {'Name':'Miel Mahmud Sifat','Product':'Apple',"Quantity":'200'},
-    {'Name':'Onik','Product':'Pen ',"Quantity":'10'},
-    {'Name':'Abrar','Product':'Pad',"Quantity":'1'},
-    {'Name':'Sulaiman','Product':'Sticker',"Quantity":'3000'},
-    {'Name':'Ovi','Product':'Mouse',"Quantity":'8'},
-    {'Name':'Imran ','Product':'Toy',"Quantity":'800'},
-    {'Name':'Rifat','Product':'Strawberry',"Quantity":'500'},
-    {'Name':'Sahari','Product':'Water',"Quantity":'56'},
-    {'Name':'Arif','Product':'Chair',"Quantity":'1'},
-    {'Name':'Emon','Product':'Banana',"Quantity":'50'},
+  dynamic  miel =[{'Name':'Miel Mahmud Sifat','Product':'Apple',"Quantity":200},
+    {'Name':'Miel Mahmud Sifat','Product':'Apple',"Quantity":200},
+    {'Name':'Onik','Product':'Pen ',"Quantity":10},
+    {'Name':'Abrar','Product':'Pad',"Quantity":1},
+    {'Name':'Sulaiman','Product':'Sticker',"Quantity":3000},
+    {'Name':'Ovi','Product':'Mouse',"Quantity":8},
+    {'Name':'Imran ','Product':'Toy',"Quantity":800},
+    {'Name':'Rifat','Product':'Strawberry',"Quantity":500},
+    {'Name':'Sahari','Product':'Water',"Quantity":56},
+    {'Name':'Arif','Product':'Chair',"Quantity":1},
+    {'Name':'Emon','Product':'Banana',"Quantity":50},
   ];
-
+  int d=0;
   for( dynamic order in miel)
     {
-      print( order['Name']);
+      int g=order['Quantity'];
+
+        d=d+g;
+
+
+
     }
+  print(d);
 
  /* for(int i=0;i<miel.length;i++)
     {
