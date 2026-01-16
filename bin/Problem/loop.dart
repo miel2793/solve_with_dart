a7380df9e3e6109fc7d0977668e9b6834cb0104d
miel2793 <mahmudsifat2793@gmail.com>
2026-01-16 11:08:35 +0600

    add

--- bin/Problem/loop.dart
+++ bin/Problem/loop.dart
@@ -1,7 +1,15 @@
 main()
 {
-  for(int i =0;i<100;i++)
+  /*for(int i =0;i<100;i++)
     {
       print('${i*8}');
-    }
+    }*/
+
+   List<String> student = ['Miel','Ovi','Almas','Robin','Emon','Utsho','Sulaiman','Abrar'];
+
+   for(int i= 0;i<student.length;i++)
+     {
+       print(" Your Child ${student[i]}  is nor present  Today");
+     }
+
 }
--- bin/Problem/map.dart
+++ bin/Problem/map.dart
@@ -12,6 +12,23 @@ main()
   Set s ={ 'miel','afo','kfli'};
   s.add('afi');
 
-  print(s.elementAt(2));
+  var  miel =[{'Name':'Miel Mahmud Sifat','Product':'Apple',"Quantity":'200'},
+    {'Name':'Miel Mahmud Sifat','Product':'Apple',"Quantity":'200'},
+    {'Name':'Onik','Product':'Pen ',"Quantity":'10'},
+    {'Name':'Abrar','Product':'Pad',"Quantity":'1'},
+    {'Name':'Sulaiman','Product':'Sticker',"Quantity":'3000'},
+    {'Name':'Ovi','Product':'Mouse',"Quantity":'8'},
+    {'Name':'Imran ','Product':'Toy',"Quantity":'800'},
+    {'Name':'Rifat','Product':'Strawberry',"Quantity":'500'},
+    {'Name':'Sahari','Product':'Water',"Quantity":'56'},
+    {'Name':'Arif','Product':'Chair',"Quantity":'1'},
+    {'Name':'Emon','Product':'Banana',"Quantity":'50'},
+  ];
+
+  for(int i=0;i<miel.length;i++)
+    {
+      print('The Customer Name Is ${miel[i]['Name']}, purchased ${miel[i]['Product']}, and total quantity is ${miel[i]['Quantity']}');
+    }
 
 }
+
