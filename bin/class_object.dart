c440eb3e18681cad960d34c4d269f0d10a11d73f
miel2793 <mahmudsifat2793@gmail.com>
2026-01-23 12:01:29 +0600

    add

---
+++ bin/class_object.dart
@@ -0,0 +1,21 @@
+import 'dart:io';
+class Student
+ {
+
+   String ? name;
+    String ? dept;
+     int ? id;
+      int ? intake;
+      int?  section;
+
+ }
+ main()
+ {
+   Student  s = Student();
+   s.name ='Miel Mahmud Sifat';
+   
+   
+   
+
+
+ }
