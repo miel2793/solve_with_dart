949bae30e9294d929655d07a12e3c05180664b06
miel2793 <mahmudsifat2793@gmail.com>
2026-01-26 21:19:55 +0600

    add

---
+++ bin/Problem/get_set problem/main.dart
@@ -0,0 +1,13 @@
+
+import 'p1.dart';
+ main()
+{
+
+
+  Student stu =Student();
+  stu.setname='miel';
+
+  print(stu.getname);
+
+}
+
---
+++ bin/Problem/get_set problem/p1.dart
@@ -0,0 +1,18 @@
+  class Student
+  {
+
+     String  ?  _name;
+
+
+      set  setname(String newname)
+      {
+         _name=newname;
+      }
+
+     get getname
+     {
+       return _name;
+     }
+
+
+  }
