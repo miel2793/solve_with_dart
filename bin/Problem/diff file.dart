24c99633d5211d61f0ae2c9008a3343a905ebe73
miel2793 <mahmudsifat2793@gmail.com>
2026-01-26 14:34:26 +0600

    add

--- bin/Problem/diff file.dart
+++ bin/Problem/diff file.dart
@@ -2,7 +2,13 @@ class   Ali
 {
   String  _name ="Ali ahmed";
 
+  int _price=22;
+
        get hdk{
         return _name;
      }
+
+      get ConcurrentModificationError{
+          return _price;
+      }
 }
