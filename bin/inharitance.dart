cef62038eef1a1819acc2c43410fa6231386a062
miel2793 <mahmudsifat2793@gmail.com>
2026-01-26 11:25:04 +0600

    add

---
+++ bin/inharitance.dart
@@ -0,0 +1,20 @@
+class Father
+{
+     String ? land;
+      String ? address;
+
+
+
+}
+ class    Chulu  extends Father
+
+ {
+    String ?  name;
+
+ }
+  main()
+  {
+     Chulu  miel = Chulu();
+     print(miel.land=' 10 bigha');
+
+  }
